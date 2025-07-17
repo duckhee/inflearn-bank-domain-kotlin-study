692d519cc77047adf8cfafa2e542d10f31aaadc0
DoukheeWon <fain9301@yahoo.com>
2025-07-17 11:45:54 +0900

    feat: set event publisher add

--- bank-event/src/main/kotlin/publisher/EventPublisher.kt
+++ bank-event/src/main/kotlin/publisher/EventPublisher.kt
@@ -1,8 +1,7 @@
 package kr.co.won.bank.event.publisher
 
-import com.sun.org.slf4j.internal.Logger
-import com.sun.org.slf4j.internal.LoggerFactory
 import kr.co.won.bank.domain.event.DomainEvent
+import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
@@ -25,7 +24,7 @@ class EventPublisherImpl(
     private val eventPublisher: ApplicationEventPublisher, // spring에서 제공하는 Event Publisher -> Application 내부에서 이벤트 발행 및 처리하기 위한 부분 외부의 시스템과 처리하기 위해서는 Redis나 RabbitMQ, Kafka를 이용해서 처리해야 한다.
 ) : EventPublisher {
 
-    private val logger: Logger = LoggerFactory.getLogger(EventPublisherImpl::class.java)
+    private val logger = LoggerFactory.getLogger(EventPublisherImpl::class.java)
 
     override fun publish(event: DomainEvent) {
         eventPublisher.publishEvent(event)
